54c1fab700ca717172d0904f8a1dc1a445a76ef6
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2018-04-25 10:32:03 +0300

    Add greeter class with Greet method

---
+++ HelloWorld/Greeter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+	class Greeter
+	{
+		public void Greet(string Whom)
+		{
+			Console.WriteLine("Hello " + Whom);
+		}
+	}
+}
